7a48749ad34fd04f4c773c37804b70ea336cb82b
林奕忠 <lin1987www@gmail.com>
2020-02-13 11:26:18 +0800

    v0.0.1

--- test/enzyme.test.js
+++ test/enzyme.test.js
@@ -1,7 +1,7 @@
 import {shallow, mount, render} from 'enzyme'; // eslint-disable-line no-unused-vars
 import React from 'react';
 import {assert, expect, should} from 'chai'; // eslint-disable-line no-unused-vars
-import WrappedApp from '../src/web/hi_i18n.jsx';
+import WrappedApp from '../src/views/hi_i18n.jsx';
 
 // Only run under mocha(node)
 
